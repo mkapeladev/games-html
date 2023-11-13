6f0468475af626525d13b925ebcedadbfbce1475
mkapeladev <maciusk12@gmail.com>
2023-11-13 22:32:16 +0100

    w

--- Pong/main.js
+++ Pong/main.js
@@ -1,20 +1,40 @@
+//calling variables
 var cnv;
 var ctx;    
-
-ballx = 400;
-bally = 300;
-ballsy = 3;
-ballsx = 5;
+var ballx = 400;
+var bally = 300;
+var ballsy = 3;
+var ballsx = 5;
+var p1Y = 200;
+const P1_HEIGHT = 200;
 
 window.onload = function(){
 cnv = document.getElementById("cnv1")
 ctx = cnv.getContext("2d");
 var fps = setInterval('onFrame()',  1000/60)
+cnv.addEventListener("mousemove", function(evt){
+   var mp  = MousePosition(evt)
+   p1Y = mp.y - (P1_HEIGHT/2) ;
+   console.log(p1Y)
+})
+}
+
+function MousePosition(evt){
+   var rect = cnv.getBoundingClientRect();
+   var root = document.documentElement;
+   var mX = evt.clientX - rect.left - root.scrollLeft;
+   var mY = evt.clientY - rect.top - root.scrollTop;
+   return {
+      x:mX,
+      y:mY
+   }
+   
 }
 
 function onFrame(){
    draw()
    move()
+   
 }
 
 function move(){
@@ -42,7 +62,7 @@ function crarc(x,y,rad,sagle,eangle,cl){
 }
 function draw(){
     colorRect(0 , 0 , cnv.width , cnv.height, 'black')
-    colorRect(10, 200, 10, 200, "white")
+    colorRect(10, p1Y, 10, P1_HEIGHT, "white")
     crarc(ballx, bally, 10, 10, Math.PI, true)
 
 
